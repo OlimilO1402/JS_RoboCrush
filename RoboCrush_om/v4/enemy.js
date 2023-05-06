7822ea877563e99fb2f58d459a2fe0670442cf76
OlimilO1402 <olimilo@gmx.net>
2023-05-06 11:51:08 +0200

    update class syntax in Enemy-class

--- RoboCrush_om/v4/enemy.js
+++ RoboCrush_om/v4/enemy.js
@@ -1,31 +1,36 @@
 //==================   Enemy   ==================//
-/**
- * creates a new enemy
- * @param {GraphicPicture} gpic
- * @param {Number} speed
- * @param {Bullets} bullets
- */
-function Enemy(gpic, speed, bullets)
+class Enemy
 {
-    this.Life = 30;
-    this.GPic = gpic;
-    this.Speed = speed;
-    this.Bombs = bullets;
+    /**
+     * creates a new enemy
+     * @param {GraphicPicture} gpic
+     * @param {Number} speed
+     * @param {Bullets} bullets
+     */    
+    constructor(gpic, speed, bullets)
+    {
+        this.Life = 30;
+        this.GPic = gpic;
+        this.Speed = speed;
+        this.Bombs = bullets;
+    }
+
+    /**
+     * moves the enemy, they are only allowed to move from left to right  
+     */
+    move()
+    {
+        //by definition only moves from left to right in x-direction
+        this.GPic.moveDxy(this.Speed, 0);
+    }
+
+    /**
+     * the enemy shoots by dropping a bomb 
+     */
+    shoot()
+    {
+        var x = this.GPic.AARectDst.right();
+        var y = this.GPic.AARectDst.Point.Y;
+        this.Bombs.add(x, y, true);
+    }
 }
-/**
- * moves the enemy, they are only allowed to move from left to right  
- */
-Enemy.prototype.move = function()
-{
-    //by definition only moves from left to right in x-direction
-    this.GPic.moveDxy(this.Speed, 0);
-};
-/**
- * the enemy shoots, drops a bomb 
- */
-Enemy.prototype.shoot = function()
-{
-    var x = this.GPic.AARectDst.right();
-    var y = this.GPic.AARectDst.Point.Y;
-    this.Bombs.add(x, y, true);
-};
